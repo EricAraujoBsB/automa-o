eb8f7ce6a2e78a2d748be18910ef23cfad5caebe
EricAraujoBsB <egericaraujo@gmail.com>
2025-08-20 17:55:14 -0300

    complementação ISE

--- ISE.py
+++ ISE.py
@@ -2,61 +2,56 @@ from selenium import webdriver
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from pathlib import Path
-
-current_dir = Path(__file__).parent
-
-from data.conexao import inserir_valor_bd
-
 
 navegador = webdriver.Chrome()
-navegador.maximize_window()
 
 navegador.get("https://10.2.140.101/admin/login.jsp")
-
-
-karspesky = navegador.find_element("id", "karspersky").click()
-
-
-campo_user = navegador.find_element("id", "user")
+navegador.maximize_window()
+time.sleep(3)
+campo_user = navegador.find_element("id", "dijit_form_TextBox_0")
 campo_user.send_keys("eric.araujo")
 
-campo_senha = navegador.find_element("id", "password")
-campo_senha.send_keys("*************$Bv")
+campo_senha = navegador.find_element("id", "dijit_form_TextBox_1")
+campo_senha.send_keys("91931204$Bv")
 
-login = navegador.find_element("class name", "login").click()
+login = navegador.find_element("id", "loginPage_loginSubmit")
+login.click()
+
+time.sleep(10)
+btn_carrosel = navegador.find_element("id", "carousel-next")
+espera = WebDriverWait(navegador, 20)
+espera.until(ec.element_to_be_clickable(btn_carrosel))
+
+btn_carrosel.click()
+time.sleep(2)
+btn_carrosel.click()
+time.sleep(2)
+btn_carrosel.click()
+time.sleep(2)
+btn_carrosel.click()
+time.sleep(2)
+
+lista_titulos = navegador.find_elements("class name", "dashlet-title")
+lista_values = navegador.find_elements("class name", "metric-value ")
+
+for titulo in lista_titulos:
+    for value in lista_values:
+
+        print(titulo.text)
+
+        titulo_activate = titulo.text
+        if titulo_activate == "Active Endpoints":
+            value_actual = value.text
+            print(value_actual)
+            time.sleep(5)
+        break
+        # else:
+        #     print("activate endpoint não localizado")
 
 
-try:
-    while True:
-        try:
-            # Tentando localizar o elemento
-            elemento = navegador.find_element("class name", "activate-endpoints")
-            atual = elemento.text
-            if atual is not None:
-                try:
-                    inserir_valor_bd()
-            else:
+# try:
+#     while True:
+#         try:
+#             Activate = navegador.find_element()
 
-            # Aqui você pode adicionar qualquer lógica para lidar com o estado do elemento
-            if elemento.is_displayed():
-                print("Elemento encontrado e visível!")
-            else:
-                print("Elemento encontrado, mas não está visível.")
-        except Exception as e:
-            print(f"Erro ao tentar encontrar o elemento: {e}")
-
-        # Aguarda 1 minuto antes de verificar novamente
-        time.sleep(60)
-
-except KeyboardInterrupt:
-    print("Monitoramento interrompido pelo usuário.")
-
-finally:
-    # Fechar o driver após o término do monitoramento
-    driver.quit()
-
-
-
-
-time.sleep(20)
+time.sleep(10)
